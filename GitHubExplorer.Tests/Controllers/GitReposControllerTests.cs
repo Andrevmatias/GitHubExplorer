6fb951f5ddc532c1a50bb83cf0b44e18f4e2a134
André Victória Matias <andre@progenese.com.br>
2020-03-03 19:04:48 -0300

    Organização de pastas e adição de projeto de teste

---
+++ GitHubExplorer.Tests/Controllers/GitReposControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using GitHubExplorer.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitHubExplorer.Controllers.Tests
+{
+    public class GitReposControllerTests
+    {
+        [Fact()]
+        public void GitReposControllerTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void SearchTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void SearchByUserTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void GetTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
