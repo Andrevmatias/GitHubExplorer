15d5e61e3a041db6cf9d228228772d7ec9704f2f
André Victória Matias <andre@progenese.com.br>
2020-03-03 20:21:00 -0300

    Adição de testes para o serviço do GitHub

---
+++ GitHubExplorer.App/Services/GitHubReposService.cs
@@ -0,0 +1,26 @@
+﻿using GitHubExplorer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitHubExplorer.Services
+{
+    public class GitHubReposService : IGitReposService
+    {
+        public GitRepo GetRepo(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Page<GitRepoListItem> GetReposPage(string filter, int page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Page<GitRepoListItem> GetUserReposPage(string userId, int page)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GitHubExplorer.Tests/Services/GitHubReposServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+using GitHubExplorer.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitHubExplorer.Services.Tests
+{
+    public class GitHubReposServiceTests
+    {
+        [Fact()]
+        public void GetRepoTest()
+        {
+            var service = new GitHubReposService();
+
+            var repoId = "Andrevmatias/GitHubExplorer";
+
+            var repo = service.GetRepo(repoId);
+
+            Assert.Equal("GitHubExplorer", repo.Name);
+
+            var nullRepo = service.GetRepo("NONEXISTENTID_____________I_HOPE");
+
+            Assert.Null(nullRepo);
+        }
+
+        [Fact()]
+        public void GetReposPageTest()
+        {
+            var service = new GitHubReposService();
+
+            var repos = service.GetUserReposPage("GitHubExplorer", 1);
+
+            Assert.NotEmpty(repos.Items);
+        }
+
+        [Fact()]
+        public void GetUserReposPageTest()
+        {
+            var service = new GitHubReposService();
+
+            var repos = service.GetUserReposPage("Andrevmatias", 1);
+
+            Assert.NotEmpty(repos.Items);
+        }
+    }
+}
